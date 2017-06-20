4db727348aa1927e02b382370c2e8f2276652ac4
codelabor <codelabor@gmail.com>
2017-06-20 16:57:31 +0900

    Init

---
+++ src/main/java/org/codelabor/example/helloworld/service/HelloWorldServiceImpl2.java
@@ -0,0 +1,9 @@
+package org.codelabor.example.helloworld.service;
+
+public class HelloWorldServiceImpl2 implements HelloWorldService {
+
+	public String sayHello() {
+		return "Hello, World 2!";
+	}
+
+}
--- src/test/java/org/codelabor/example/context/support/ClassPathXmlApplicationContextTest.java
+++ src/test/java/org/codelabor/example/context/support/ClassPathXmlApplicationContextTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -28,8 +29,62 @@ public class ClassPathXmlApplicationContextTest {
 	}
 
 	@Test
-	public void testClassPathXmlApplicationContextString() {
-		String configLocation = "";
+	public void testClassPathXmlApplicationContextString1() {
+		String configLocation = "applicationContext1.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextString2() {
+		String configLocation = "applicationContext2.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringAbsPath1() {
+		String configLocation = "module1/applicationContext1.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringAbsPath2() {
+		String configLocation = "module2/applicationContext2.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithPrefix1() {
+		String configLocation = "classpath:applicationContext1.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithPrefix2() {
+		String configLocation = "classpath:applicationContext2.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithWildcardPrefix1() {
+		String configLocation = "classpath*:applicationContext1.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithWildcardPrefix2() {
+		String configLocation = "classpath*:applicationContext2.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithWildcardPrefix3() {
+		String configLocation = "classpath*:applicationContext3.xml";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextStringWithWildcardPrefix4() {
+		String configLocation = "classpath*:applicationContext4.xml";
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
 	}
 

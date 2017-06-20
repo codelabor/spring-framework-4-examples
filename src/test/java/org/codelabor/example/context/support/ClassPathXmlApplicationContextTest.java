3dcf09a4605c559b4683dbd66b898bd7d7fb79fb
codelabor <codelabor@gmail.com>
2017-06-20 15:46:21 +0900

    Init

---
+++ src/test/java/org/codelabor/example/context/support/ClassPathXmlApplicationContextTest.java
@@ -0,0 +1,36 @@
+package org.codelabor.example.context.support;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ClassPathXmlApplicationContextTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testClassPathXmlApplicationContextString() {
+		String configLocation = "";
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	}
+
+}

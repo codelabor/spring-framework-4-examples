7c1d26edb039df5b34da70e967c0ce989b4e5381
Shin Sang-jae <codelabor@gmail.com>
2017-06-22 13:32:15 +0900

    예제 보완

--- src/test/java/org/codelabor/example/context/support/ClassPathXmlApplicationContextTest.java
+++ src/test/java/org/codelabor/example/context/support/ClassPathXmlApplicationContextTest.java
@@ -1,16 +1,20 @@
 package org.codelabor.example.context.support;
 
-import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
 
+import org.codelabor.example.helloworld.service.HelloWorldService;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ClassPathXmlApplicationContextTest {
+	private static final Logger logger = LoggerFactory.getLogger(ClassPathXmlApplicationContextTest.class);
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -31,61 +35,67 @@ public class ClassPathXmlApplicationContextTest {
 	@Test
 	public void testClassPathXmlApplicationContextString1() {
 		String configLocation = "applicationContext1.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
-	}
-
-	@Test
-	public void testClassPathXmlApplicationContextString2() {
-		String configLocation = "applicationContext2.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+		}
 	}
 
 	@Test
 	public void testClassPathXmlApplicationContextStringAbsPath1() {
 		String configLocation = "module1/applicationContext1.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
-	}
-
-	@Test
-	public void testClassPathXmlApplicationContextStringAbsPath2() {
-		String configLocation = "module2/applicationContext2.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		}
 	}
 
 	@Test
 	public void testClassPathXmlApplicationContextStringWithPrefix1() {
 		String configLocation = "classpath:applicationContext1.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+		}
 	}
 
 	@Test
-	public void testClassPathXmlApplicationContextStringWithPrefix2() {
-		String configLocation = "classpath:applicationContext2.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	public void testClassPathXmlApplicationContextStringAbsPathWithPrefix1() {
+		String configLocation = "classpath:module1/applicationContext1.xml";
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		}
 	}
 
 	@Test
 	public void testClassPathXmlApplicationContextStringWithWildcardPrefix1() {
 		String configLocation = "classpath*:applicationContext1.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+		}
 	}
 
 	@Test
-	public void testClassPathXmlApplicationContextStringWithWildcardPrefix2() {
-		String configLocation = "classpath*:applicationContext2.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
-	}
-
-	@Test
-	public void testClassPathXmlApplicationContextStringWithWildcardPrefix3() {
-		String configLocation = "classpath*:applicationContext3.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
-	}
-
-	@Test
-	public void testClassPathXmlApplicationContextStringWithWildcardPrefix4() {
-		String configLocation = "classpath*:applicationContext4.xml";
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation);
+	public void testClassPathXmlApplicationContextStringAbsPathWithWildcardPrefix1() {
+		String configLocation = "classpath*:module1/applicationContext1.xml";
+		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configLocation)) {
+			HelloWorldService helloWorldService = applicationContext.getBean("helloWorldService1",
+					HelloWorldService.class);
+			logger.info(helloWorldService.sayHello());
+		}
 	}
 
 }

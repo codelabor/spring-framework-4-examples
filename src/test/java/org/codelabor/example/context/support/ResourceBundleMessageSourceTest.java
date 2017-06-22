5377fd02c0dde50ba8806bafe08dcb9957ac517d
codelabor <codelabor@gmail.com>
2017-06-22 16:59:46 +0900

    Init

---
+++ src/test/java/org/codelabor/example/context/support/ResourceBundleMessageSourceTest.java
@@ -0,0 +1,24 @@
+package org.codelabor.example.context.support;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResourceBundleMessageSourceTest {
+
+	@Test
+	public void testGetMessageStringObjectArrayStringLocale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMessageStringObjectArrayLocale() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMessageMessageSourceResolvableLocale() {
+		fail("Not yet implemented");
+	}
+
+}
